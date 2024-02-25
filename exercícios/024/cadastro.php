caab8461b69f06d8b0c7bfbf59db69d2d42597c5
Felipe <fefelbf@gmail.com>
2024-02-24 21:07:28 -0300

    comit

---
+++ exercícios/024/cadastro.php
@@ -0,0 +1,3 @@
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio esse, labore laboriosam temporibus
+saepe molestias ipsam? Ducimus vero quibusdam vel provident debitis, 
+sed fuga earum molestiae voluptatem a, cum perferendis.
